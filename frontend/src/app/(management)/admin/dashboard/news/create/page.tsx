0cebbd14ed8b4ebd22f63993b53ae99a5c0b3f50
Benjamin Okumu <okumub85@gmail.com>
2025-08-15 18:37:13 +0300

    design update

--- frontend/src/app/(management)/admin/dashboard/news/create/page.tsx
+++ frontend/src/app/(management)/admin/dashboard/news/create/page.tsx
@@ -5,7 +5,7 @@ import NewsArticleForm from "@/components/admin/news/create/NewsArticleForm";
 export default function CreateNewsPage() {
   return (
     <div className="p-4 mx-auto">
-      <h1 className="text-2xl font-bold mb-6">Create News Article</h1>
+      {/* <h1 className="text-2xl font-bold mb-6">Create News Article</h1> */}
       <NewsArticleForm />
     </div>
   );
